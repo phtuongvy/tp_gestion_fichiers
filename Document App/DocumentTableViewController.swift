6e8c91b255b556640d727f5f5a4e036527ab3b0a
phtuongvy <tuong-vy-pham-huynh@etu.univ-smb.fr>
2024-11-18 11:46:43 +0100

    Add some data in DocumentTableViewController

---
+++ Document App/DocumentTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  DocumentTableViewController.swift
+//  Document App
+//
+//  Created by Pham Huynh Tuong Vy on 18/11/2024.
+//
+
+import UIKit
+
+struct Document {
+    let title: String
+    let subtitle: String
+}
+
+class DocumentTableViewController: UITableViewController {
+    
+    let documents: [Document] = [
+        Document(title: "Document 1", subtitle: "Subtitle 1"),
+        Document(title: "Document 2", subtitle: "Subtitle 2"),
+        Document(title: "Document 3", subtitle: "Subtitle 3"),
+        Document(title: "Document 4", subtitle: "Subtitle 4"),
+        Document(title: "Document 5", subtitle: "Subtitle 5")
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        tableView.register( UITableViewCell.self, forCellReuseIdentifier: "DocumentCell")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        // #warning Incomplete implementation, return the number of rows
+        
+        return documents.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
+
+        cell.textLabel?.text = documents[ indexPath.row ].title
+        cell.detailTextLabel?.text = documents[ indexPath.row ].subtitle
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Notes.md
+++ Notes.md
@@ -1,1 +1,14 @@
 
+### Comment basculer Table View Controller en tant que Initial View Controller ?
+
+- Sélectionner la Table View Controller et ensuite dans l'inspecteur d'attributs, il faut cocher Is Initial View Controller dans l'onglet View Controller.
+
+### Comment enregistrer un identifiant d'un composant du storyboard dans un ViewController ?
+
+- Ajouter cette ligne :
+
+tableView.register( UITableViewCell.self, forCellReuseIdentifier: "DocumentCell")
+
+dans override func viewDidLoad() {}
+
+
